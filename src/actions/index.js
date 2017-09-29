b37f395dfa8e4509758d28294a762e6170c95136
Lamsal <n0312809@lm.lmig.com>
2017-09-29 11:58:30 -0400

    can show all the data in the table now

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const API_KEY = 'f168a3e9dd078087857f1115286df64b';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export default function(city) {
-  const url = `${ROOT_URL}&q={city},us`;
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
 
   return {
---
+++ src/reducers/reducer_weather.js
@@ -0,0 +1,10 @@
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      return [action.payload.data, ...state];
+    //same as return state.concat([action.payload.data])
+  }
+  return state;
+}
